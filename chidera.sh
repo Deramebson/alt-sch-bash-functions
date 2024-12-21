7dc8b728220bcc2fb22fdfc00569403c248f279d
Deramebson <deramebson@gmail.com>
2024-12-21 23:44:06 +0100

    chideraassignment

---
+++ chidera.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Function to display full name
+fullname() {
+  echo "Fullname: Chidera Mebuge"
+}
+
+# Function to display bio
+bio() {
+  echo "Bio: Keen on solving problems and making an impact."
+}
+
+# Function to display year in tech
+year_in_tech() {
+  echo "Year in Tech: 2 years"
+}
+
+# Function to display dream job
+dream_job() {
+  echo "Dream Job: Cloud Solutions Architect"
+}
+
+# Function to display goals
+goals() {
+  echo "Goals ⚽: 
+  - Master cloud platforms
+  - Build cloud infrastructure projects
+  - Network and collaborate with other teams
+  - Start cloud consulting for businesses"
+}
+
+# Call functions to display all the information
+who_am_i() {
+  echo "Hello, I am the Chidera Mebuge🎉"
+  fullname
+  bio
+  year_in_tech
+  dream_job
+  goals
+}
